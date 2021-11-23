8f0d03c33373a219341481782837ea1f7425a923
Kazuto Kusama <jacopen@gmail.com>
2021-11-23 08:15:56 +0000

    add s3

--- fastly.tf
+++ fastly.tf
@@ -5,6 +5,13 @@ resource "fastly_service_v1" "prod" {
     name    = "prod.udcp.info"
     comment = "service for production"
   }
+  
+  backend {
+    address = aws_s3_bucket.jacopen_fastly_test.website_endpoint
+    name    = "website"
+    port    = "80"
+    override_host = aws_s3_bucket.jacopen_fastly_test.website_endpoint
+  }
 
   force_destroy = true
 }
---
+++ s3.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "jacopen_fastly_test" {
+  bucket = "jacopen-fastly-website"
+  acl    = "public-read"
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "jacopen_fastly_test" {
+  bucket = aws_s3_bucket.jacopen_fastly_test.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "bucketpolicy"
+    Statement = [
+      {
+        Sid       = "AddPerm"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          "${aws_s3_bucket.jacopen_fastly_test.arn}/*"
+        ]
+      },
+    ]
+  })
+}
+
+resource "null_resource" "remove_and_upload_to_s3" {
+  provisioner "local-exec" {
+    command = "aws s3 sync ${path.module}/website s3://${aws_s3_bucket.jacopen_fastly_test.id}"
+  }
+}
+
