8a72a1cfe64565b29aa043de42ef899375c8c482
Kazuto Kusama <jacopen@gmail.com>
2021-11-23 08:10:45 +0000

    First

--- fastly.tf
+++ fastly.tf
@@ -5,26 +5,6 @@ resource "fastly_service_v1" "prod" {
     name    = "prod.udcp.info"
     comment = "service for production"
   }
-  
-  backend {
-    address = aws_s3_bucket.jacopen_fastly_test.website_endpoint
-    name    = "website"
-    port    = "80"
-    override_host = aws_s3_bucket.jacopen_fastly_test.website_endpoint
-  }
-
-  backend {
-    address = aws_s3_bucket.jacopen_fastly_ui.website_endpoint
-    name    = "ui"
-    port    = "80"
-    override_host = aws_s3_bucket.jacopen_fastly_ui.website_endpoint
-  }
-
-  vcl {
-    name    = "custom_vcl"
-    content = "${file("${path.module}/vcl/custom.vcl")}"
-    main    = true
-  }
 
   force_destroy = true
 }
