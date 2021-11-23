02bafc1551a2af8849371571975d4e90f7757cb4
Kazuto Kusama <jacopen@gmail.com>
2021-11-23 11:08:15 +0000

    Remove UI

--- s3.tf
+++ s3.tf
@@ -31,39 +31,3 @@ resource "null_resource" "remove_and_upload_to_s3" {
     command = "aws s3 sync ${path.module}/website s3://${aws_s3_bucket.jacopen_fastly_test.id}"
   }
 }
-
-
-resource "aws_s3_bucket_policy" "jacopen_fastly_ui" {
-  bucket = aws_s3_bucket.jacopen_fastly_ui.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "bucketpolicy"
-    Statement = [
-      {
-        Sid       = "AddPerm"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource = [
-          "${aws_s3_bucket.jacopen_fastly_ui.arn}/*"
-        ]
-      },
-    ]
-  })
-}
-
-
-resource "aws_s3_bucket" "jacopen_fastly_ui" {
-  bucket = "jacopen-fastly-ui"
-  acl    = "public-read"
-  website {
-    index_document = "index.html"
-  }
-}
-
-resource "null_resource" "remove_and_upload_to_s3_ui" {
-  provisioner "local-exec" {
-    command = "aws s3 sync ${path.module}/ui s3://${aws_s3_bucket.jacopen_fastly_ui.id}"
-  }
-}
