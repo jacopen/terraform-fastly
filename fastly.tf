c2d7691f6baf6c350ce23ba495b749776a978f8d
Kazuto Kusama <jacopen@gmail.com>
2021-11-23 08:16:41 +0000

    Add s3 and VCL

--- fastly.tf
+++ fastly.tf
@@ -13,5 +13,18 @@ resource "fastly_service_v1" "prod" {
     override_host = aws_s3_bucket.jacopen_fastly_test.website_endpoint
   }
 
+  backend {
+    address = aws_s3_bucket.jacopen_fastly_ui.website_endpoint
+    name    = "ui"
+    port    = "80"
+    override_host = aws_s3_bucket.jacopen_fastly_ui.website_endpoint
+  }
+
+  vcl {
+    name    = "custom_vcl"
+    content = "${file("${path.module}/vcl/custom.vcl")}"
+    main    = true
+  }
+
   force_destroy = true
 }
