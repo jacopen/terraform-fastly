181a25f31884ea1494608d8f2d1b72af88c6a7f1
Kazuto Kusama <jacopen@gmail.com>
2021-11-23 07:03:13 +0000

    Update

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    fastly = {
+      source = "fastly/fastly"
+      # version = "~> 0.38.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "fastly" {
+
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
